d24c245b01e432ab4c50da65b0b2378fd080e2e7
David <66916485+tDavidM@users.noreply.github.com>
2025-03-04 20:56:40 -0500

    small mistake
    some dumb mistake that got lost between the french version that I tested and use daily versus the public english version that I didn't test properly

--- ProjectExchange.cpp
+++ ProjectExchange.cpp
@@ -5,6 +5,7 @@
 //---------------------------------------------------------------------------
 USEFORM("SelPassKey.cpp", fSelPassKey);
 USEFORM("MainExchange.cpp", MainForm);
+USEFORM("Clipboard.cpp", fClipboard);
 //---------------------------------------------------------------------------
 WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
@@ -15,6 +16,7 @@ WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		Application->Title = "Exchange";
       Application->CreateForm(__classid(TMainForm), &MainForm);
        Application->CreateForm(__classid(TfSelPassKey), &fSelPassKey);
+       Application->CreateForm(__classid(TfClipboard), &fClipboard);
        Application->Icon = Application->MainForm->Icon;
       Application->Run();
    }
